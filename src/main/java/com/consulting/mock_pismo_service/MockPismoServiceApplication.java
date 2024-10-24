0879e085e16eab66e93706e29ebd7c5aa599194d
marcosxavierdev <sis.marcosxavier@gmail.com>
2024-10-24 12:20:52 -0300

    first commit

---
+++ src/main/java/com/consulting/mock_pismo_service/MockPismoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.consulting.mock_pismo_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockPismoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockPismoServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/consulting/mock_pismo_service/MockPismoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.consulting.mock_pismo_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockPismoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
